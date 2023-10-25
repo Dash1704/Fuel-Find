8361ba53dfaca183ae7e351a3c93bcd3efd525f2
dashboy <dashiellboyles@hotmail.com>
2023-10-25 11:10:48 +0100

    added extra fuel stations

--- hooks/useData.ts
+++ hooks/useData.ts
@@ -1,12 +1,3 @@
-// import { fetchApi } from "./hooks/fetchApi";
-// import { FuelList } from "../types";
-
-// export function useData() {
-//   return {
-//     fuelStations: fetchApi.DataItems.FuelStationList as FuelList[],
-//   };
-// }
-
 import data from "../data.json";
 import { FuelList } from "../types";
 
