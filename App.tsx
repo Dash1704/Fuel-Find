e8f82b2992a363afd5d1e6ed0172043be96eb55e
dashboy <dashiellboyles@hotmail.com>
2023-10-30 14:58:05 +0000

    Removed unused import

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { FindButton } from './components/FindButton';
 import HomePage from './components/HomePage';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ResultsPage from './components/ResultsPage';
 
